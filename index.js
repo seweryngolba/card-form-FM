2c150de776dcf05ec428fd5263821600b890b409
Seweryn Golba <sgoolb@gmail.com>
2023-10-18 18:50:40 +0200

    input validation

--- index.js
+++ index.js
@@ -1,51 +1,78 @@
-const displayNumber = document.querySelector(".card-display-number");
-const displayName = document.querySelector(".display-name");
-const displayMonth = document.querySelector(".display-month");
-const displayYear = document.querySelector(".display-year");
-const displayCVC = document.querySelector(".cvc-back");
-const nameInput = document.querySelector(".input-name");
-const cardNumberInput = document.querySelector(".input-card");
-const monthInput = document.querySelector(".month-input");
-const yearInput = document.querySelector(".year-input");
-const cvcInput = document.querySelector(".cvc-input");
 const confirmBtn = document.querySelector(".confirm-button");
 
-nameInput.addEventListener("input", function () {
-  nameValue = nameInput.value;
-  displayName.textContent = nameValue;
-  if (displayName.textContent === "") {
-    displayName.textContent = "Jane Appleseed";
-  }
-});
+const fields = {
+  name: {
+    input: document.querySelector(".input-name"),
+    display: document.querySelector(".display-name"),
+    defaultValue: "Jane Appleseed",
+    errorInfo: document.querySelector(".input-name + .error-info"),
+  },
+  cardNumber: {
+    input: document.querySelector(".input-card"),
+    display: document.querySelector(".card-display-number"),
+    defaultValue: "0000 0000 0000 0000",
+    errorInfo: document.querySelector(".input-card + .error-info"),
+  },
+  cvc: {
+    input: document.querySelector(".cvc-input"),
+    display: document.querySelector(".cvc-back"),
+    defaultValue: "000",
+    errorInfo: document.querySelector(".cvc-input + .error-info"),
+  },
+  month: {
+    input: document.querySelector(".month-input"),
+    display: document.querySelector(".display-month"),
+    defaultValue: "00",
+    errorInfo: document.querySelector(".exp-date + .error-info"),
+  },
+  year: {
+    input: document.querySelector(".year-input"),
+    display: document.querySelector(".display-year"),
+    defaultValue: "00",
+    errorInfo: document.querySelector(".exp-date + .error-info"),
+  },
+};
 
-cardNumberInput.addEventListener("input", function () {
-  cardValue = cardNumberInput.value;
-  displayNumber.textContent = cardValue;
-  if (displayNumber.textContent === "") {
-    displayNumber.textContent = `0000 0000 0000 0000`;
-  }
-});
+function handleInput(event, field) {
+  const { input, display, defaultValue, errorInfo } = field;
+  let value = input.value;
 
-cvcInput.addEventListener("input", function () {
-  cvcValue = cvcInput.value;
-  displayCVC.textContent = cvcValue;
-  if (displayCVC.textContent === "") {
-    displayCVC.textContent = `000`;
+  if (field.input === fields.month.input || field.input === fields.year.input) {
+    value = value.padStart(2, "0");
   }
-});
 
-monthInput.addEventListener("input", function () {
-  monthValue = monthInput.value;
-  displayMonth.textContent = monthValue;
-  if (displayMonth.textContent === "") {
-    displayMonth.textContent = `00`;
+  if (isValidInput(field, value)) {
+    input.classList.remove("input-error");
+    errorInfo.style.visibility = "hidden";
+  } else {
+    input.classList.add("input-error");
+    errorInfo.style.visibility = "visible";
   }
-});
 
-yearInput.addEventListener("input", function () {
-  yearValue = yearInput.value;
-  displayYear.textContent = yearValue;
-  if (displayYear.textContent === "") {
-    displayYear.textContent = `00`;
+  display.textContent = value || defaultValue;
+}
+
+function isValidInput(field, value) {
+  if (field.input === fields.cardNumber.input) {
+    return value.length === 16 && value.trim() !== "";
   }
-});
+  if (field.input === fields.name.input) {
+    return value.trim() !== "" && !/\d/.test(value);
+  }
+  if (field.input === fields.month.input) {
+    return value.trim() !== "" && /^\d+$/.test(value) && value <= 12;
+  }
+  if (field.input === fields.year.input) {
+    return value.trim() !== "" && /^\d+$/.test(value);
+  }
+  if (field.input === fields.cvc.input) {
+    return value.trim() !== "" && /^\d+$/.test(value);
+  }
+
+  return true;
+}
+
+for (const key in fields) {
+  const field = fields[key];
+  field.input.addEventListener("input", (event) => handleInput(event, field));
+}
